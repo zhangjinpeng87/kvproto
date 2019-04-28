4a05742ac74836f2e3e08801c4939591d763428b
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2019-04-28 13:34:37 +0800

    resolve specified keys
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/protobuf/kvrpcpb.rs
+++ src/protobuf/kvrpcpb.rs
@@ -28,6 +28,7 @@ pub struct LockInfo {
     pub lock_version: u64,
     pub key: ::std::vec::Vec<u8>,
     pub lock_ttl: u64,
+    pub txn_size: u64,
     // special fields
     unknown_fields: ::protobuf::UnknownFields,
     cached_size: ::protobuf::CachedSize,
@@ -119,6 +120,21 @@ impl LockInfo {
     pub fn get_lock_ttl(&self) -> u64 {
         self.lock_ttl
     }
+
+    // uint64 txn_size = 5;
+
+    pub fn clear_txn_size(&mut self) {
+        self.txn_size = 0;
+    }
+
+    // Param is passed by value, moved
+    pub fn set_txn_size(&mut self, v: u64) {
+        self.txn_size = v;
+    }
+
+    pub fn get_txn_size(&self) -> u64 {
+        self.txn_size
+    }
 }
 
 impl ::protobuf::Message for LockInfo {
@@ -150,6 +166,13 @@ impl ::protobuf::Message for LockInfo {
                     let tmp = is.read_uint64()?;
                     self.lock_ttl = tmp;
                 },
+                5 => {
+                    if wire_type != ::protobuf::wire_format::WireTypeVarint {
+                        return ::std::result::Result::Err(::protobuf::rt::unexpected_wire_type(wire_type));
+                    }
+                    let tmp = is.read_uint64()?;
+                    self.txn_size = tmp;
+                },
                 _ => {
                     ::protobuf::rt::read_unknown_or_skip_group(field_number, wire_type, is, self.mut_unknown_fields())?;
                 },
@@ -174,6 +197,9 @@ impl ::protobuf::Message for LockInfo {
         if self.lock_ttl != 0 {
             my_size += ::protobuf::rt::value_size(4, self.lock_ttl, ::protobuf::wire_format::WireTypeVarint);
         }
+        if self.txn_size != 0 {
+            my_size += ::protobuf::rt::value_size(5, self.txn_size, ::protobuf::wire_format::WireTypeVarint);
+        }
         my_size += ::protobuf::rt::unknown_fields_size(self.get_unknown_fields());
         self.cached_size.set(my_size);
         my_size
@@ -191,6 +217,9 @@ impl ::protobuf::Message for LockInfo {
         }
         if self.lock_ttl != 0 {
             os.write_uint64(4, self.lock_ttl)?;
+        }
+        if self.txn_size != 0 {
+            os.write_uint64(5, self.txn_size)?;
         }
         os.write_unknown_fields(self.get_unknown_fields())?;
         ::std::result::Result::Ok(())
@@ -243,6 +272,7 @@ impl ::protobuf::Clear for LockInfo {
         self.clear_lock_version();
         self.clear_key();
         self.clear_lock_ttl();
+        self.clear_txn_size();
         self.unknown_fields.clear();
     }
 }
@@ -3605,6 +3635,7 @@ pub struct PrewriteRequest {
     pub start_version: u64,
     pub lock_ttl: u64,
     pub skip_constraint_check: bool,
+    pub txn_size: u64,
     // special fields
     unknown_fields: ::protobuf::UnknownFields,
     cached_size: ::protobuf::CachedSize,
@@ -3743,6 +3774,21 @@ impl PrewriteRequest {
     pub fn get_skip_constraint_check(&self) -> bool {
         self.skip_constraint_check
     }
+
+    // uint64 txn_size = 7;
+
+    pub fn clear_txn_size(&mut self) {
+        self.txn_size = 0;
+    }
+
+    // Param is passed by value, moved
+    pub fn set_txn_size(&mut self, v: u64) {
+        self.txn_size = v;
+    }
+
+    pub fn get_txn_size(&self) -> u64 {
+        self.txn_size
+    }
 }
 
 impl ::protobuf::Message for PrewriteRequest {
@@ -3794,6 +3840,13 @@ impl ::protobuf::Message for PrewriteRequest {
                     let tmp = is.read_bool()?;
                     self.skip_constraint_check = tmp;
                 },
+                7 => {
+                    if wire_type != ::protobuf::wire_format::WireTypeVarint {
+                        return ::std::result::Result::Err(::protobuf::rt::unexpected_wire_type(wire_type));
+                    }
+                    let tmp = is.read_uint64()?;
+                    self.txn_size = tmp;
+                },
                 _ => {
                     ::protobuf::rt::read_unknown_or_skip_group(field_number, wire_type, is, self.mut_unknown_fields())?;
                 },
@@ -3826,6 +3879,9 @@ impl ::protobuf::Message for PrewriteRequest {
         if self.skip_constraint_check != false {
             my_size += 2;
         }
+        if self.txn_size != 0 {
+            my_size += ::protobuf::rt::value_size(7, self.txn_size, ::protobuf::wire_format::WireTypeVarint);
+        }
         my_size += ::protobuf::rt::unknown_fields_size(self.get_unknown_fields());
         self.cached_size.set(my_size);
         my_size
@@ -3853,6 +3909,9 @@ impl ::protobuf::Message for PrewriteRequest {
         }
         if self.skip_constraint_check != false {
             os.write_bool(6, self.skip_constraint_check)?;
+        }
+        if self.txn_size != 0 {
+            os.write_uint64(7, self.txn_size)?;
         }
         os.write_unknown_fields(self.get_unknown_fields())?;
         ::std::result::Result::Ok(())
@@ -3907,6 +3966,7 @@ impl ::protobuf::Clear for PrewriteRequest {
         self.clear_start_version();
         self.clear_lock_ttl();
         self.clear_skip_constraint_check();
+        self.clear_txn_size();
         self.unknown_fields.clear();
     }
 }
@@ -6853,6 +6913,7 @@ pub struct ResolveLockRequest {
     pub start_version: u64,
     pub commit_version: u64,
     pub txn_infos: ::protobuf::RepeatedField<TxnInfo>,
+    pub keys: ::protobuf::RepeatedField<::std::vec::Vec<u8>>,
     // special fields
     unknown_fields: ::protobuf::UnknownFields,
     cached_size: ::protobuf::CachedSize,
@@ -6950,6 +7011,31 @@ impl ResolveLockRequest {
     pub fn get_txn_infos(&self) -> &[TxnInfo] {
         &self.txn_infos
     }
+
+    // repeated bytes keys = 5;
+
+    pub fn clear_keys(&mut self) {
+        self.keys.clear();
+    }
+
+    // Param is passed by value, moved
+    pub fn set_keys(&mut self, v: ::protobuf::RepeatedField<::std::vec::Vec<u8>>) {
+        self.keys = v;
+    }
+
+    // Mutable pointer to the field.
+    pub fn mut_keys(&mut self) -> &mut ::protobuf::RepeatedField<::std::vec::Vec<u8>> {
+        &mut self.keys
+    }
+
+    // Take field
+    pub fn take_keys(&mut self) -> ::protobuf::RepeatedField<::std::vec::Vec<u8>> {
+        ::std::mem::replace(&mut self.keys, ::protobuf::RepeatedField::new())
+    }
+
+    pub fn get_keys(&self) -> &[::std::vec::Vec<u8>] {
+        &self.keys
+    }
 }
 
 impl ::protobuf::Message for ResolveLockRequest {
@@ -6991,6 +7077,9 @@ impl ::protobuf::Message for ResolveLockRequest {
                 4 => {
                     ::protobuf::rt::read_repeated_message_into(wire_type, is, &mut self.txn_infos)?;
                 },
+                5 => {
+                    ::protobuf::rt::read_repeated_bytes_into(wire_type, is, &mut self.keys)?;
+                },
                 _ => {
                     ::protobuf::rt::read_unknown_or_skip_group(field_number, wire_type, is, self.mut_unknown_fields())?;
                 },
@@ -7017,6 +7106,9 @@ impl ::protobuf::Message for ResolveLockRequest {
             let len = value.compute_size();
             my_size += 1 + ::protobuf::rt::compute_raw_varint32_size(len) + len;
         };
+        for value in &self.keys {
+            my_size += ::protobuf::rt::bytes_size(5, &value);
+        };
         my_size += ::protobuf::rt::unknown_fields_size(self.get_unknown_fields());
         self.cached_size.set(my_size);
         my_size
@@ -7038,6 +7130,9 @@ impl ::protobuf::Message for ResolveLockRequest {
             os.write_tag(4, ::protobuf::wire_format::WireTypeLengthDelimited)?;
             os.write_raw_varint32(v.get_cached_size())?;
             v.write_to_with_cached_sizes(os)?;
+        };
+        for v in &self.keys {
+            os.write_bytes(5, &v)?;
         };
         os.write_unknown_fields(self.get_unknown_fields())?;
         ::std::result::Result::Ok(())
@@ -7090,6 +7185,7 @@ impl ::protobuf::Clear for ResolveLockRequest {
         self.clear_start_version();
         self.clear_commit_version();
         self.clear_txn_infos();
+        self.clear_keys();
         self.unknown_fields.clear();
     }
 }
